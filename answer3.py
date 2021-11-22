573b56efedf9f29dbaaae4ff6a85ffe70c8c5dad
Arjun-Prakash10 <51346935+Arjun-Prakash10@users.noreply.github.com>
2021-11-22 13:02:33 +0530

    Add files via upload

--- answer3.py
+++ answer3.py
@@ -1,54 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[55]:
-
-
 import pandas as pd
 from collections import Counter
 
-
-# In[23]:
-
+def extract_top3_chars(df):
+    temp_df = df[df['date']>'13/11/2021']
+    d = {}
+    for sentence in temp_df['caption']:
+        if type(sentence) == str:
+            for char in sentence:
+                if not char.isalnum() and char!=' ':
+                    if char in d:
+                        d[char]+=1
+                    else:
+                        d[char] =1
+    k = Counter(d)
+    top_3 = k.most_common(3)
+    top3 = []
+    for i in top_3:
+        top3.append((i[0],i[1]))
+    return top3
 
 df = pd.read_csv("Captions.csv")
-
-
-# In[26]:
-
-
 df['date'] = pd.to_datetime(df['date'])
 
-
-# In[57]:
-
-
-temp_df = df[df['date']>'13/11/2021']
-d = {}
-for sentence in temp_df['caption']:
-    if type(sentence) == str:
-        for char in sentence:
-            if not char.isalnum() and char!=' ':
-                if char in d:
-                    d[char]+=1
-                else:
-                    d[char] =1
-k = Counter(d)
-top_3 = k.most_common(3)
-top3 = []
-for i in top_3:
-    top3.append((i[0],i[1]))
-print(top3)
-
-
-# In[52]:
-
-
-d
-
-
-# In[ ]:
-
-
-
-
+print(extract_top3_chars(df))
